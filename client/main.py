609ab0ea6fabba51b1862f86ce2761059c5d05ef
Jeongmin <jason.jm.jang@gmail.com>
2025-07-18 17:56:17 -0700

    Added bar to move
    Added bar to move up and down and send the movement to the server

--- client/main.py
+++ client/main.py
@@ -1,8 +1,11 @@
 import connect
+import pygame
 import time
 import sys, os
+import pong_setup
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from shared import packet
+from Striker import striker
 
 # perhaps we need to make the client multithreaded to get and send game updates to the server. 
 def send_thread(data):
@@ -33,28 +36,51 @@ def recv_handler(conn: connect.client_connection):
 
     conn.close()
 
+def sendMovement(c, data):
+    p = packet.serialize(data, packet.Status.MOVE)
+    c.send(p)
 
 try:
     c = connect.init_connection()
     
     print(c)
 
+    pygame.init()
+
     id = c.receive().decode()
     c.set_id(id)
     print(f"Connected with ID: {id}")
-
+    
     c.start_recieving(recv_handler)
     
     # send a test packet MOVE for every 5 seconds
-    while True:
-        data = {'uuid': c.get_id(), 'x': 1.0, 'y': 2.0}
-
-        p = packet.serialize(data, packet.Status.MOVE)
-        c.send(p)
-        time.sleep(5)
-        
-
-
+    player = striker(pong_setup.WIDTH-20, 0, 10, 100, 10, pong_setup.GREEN)
+    running = True
+    move = 0
+    while running:
+        pong_setup.screen.fill(pong_setup.BLACK)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    move = -1
+                    data = {'uuid': c.get_id(), 'x': 0, 'y': -1}
+                    sendMovement(c, data)
+                if event.key == pygame.K_DOWN:
+                    move = 1
+                    data = {'uuid': c.get_id(), 'x': 0, 'y': 1}
+                    sendMovement(c, data)
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    move = 0
+                    data = {'uuid': c.get_id(), 'x': 0, 'y': 0}
+                    sendMovement(c, data)
+        player.update(move)
+        player.display()
+        pygame.display.update()
+        pong_setup.clock.tick(pong_setup.FPS)
+    pygame.quit()
 except Exception as e:
     print(e)
     exit(1)
---
+++ client/pong_setup.py
@@ -0,0 +1,19 @@
+import pygame
+
+pygame.init()
+
+# Font that is used to render the text
+font20 = pygame.font.Font('freesansbold.ttf', 20)
+
+# RGB values of standard colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+
+# Basic parameters of the screen
+WIDTH, HEIGHT = 900, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pong")
+# Used to adjust the frame rate
+clock = pygame.time.Clock()
+FPS = 30
